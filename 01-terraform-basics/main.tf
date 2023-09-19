4b64d07c5ec41c0f35f9300618ed3a1ffd1e98eb
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-19 17:32:27 +0100

    Update terraform syntax

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
+  # VERSION IS NOT NEEDED HERE
 }
