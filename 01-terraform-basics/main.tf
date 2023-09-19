c48d6b45a2045bf8d2f297cec01c41cd0d534af5
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-19 18:02:58 +0100

    Create S3 Bucket

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -2,12 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
+  
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
   # VERSION IS NOT NEEDED HERE
+  #Put access to the provider via environment variable instead of hardcoded -> https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 }
+
+#Plan -> Apply
+
+##Create bucket/Storage
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-badalo-123"
+}
+
