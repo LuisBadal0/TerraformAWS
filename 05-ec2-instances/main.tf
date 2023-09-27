15af5daae06658fbd2d1b261ca56573ce097fcd7
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-27 22:15:14 +0100

    Install http server on EC2

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -8,6 +8,10 @@ terraform {
 
 }
 
+variable "aws_key_pair" {
+  default = "~/aws/aws_keys/default-ec2.pem"
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
@@ -51,9 +55,24 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  ami                        = "ami-0b25f6ba2f4419235"
-  key_name                   = "default-ec2"
-  instance_type              = "t2.micro"
+  ami                    = "ami-0b25f6ba2f4419235"
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
-  subnet_id                  = "subnet-0ab30df0d626c6175"
+  subnet_id              = "subnet-0ab30df0d626c6175"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",                                                             //install httpd
+      "sudo service httpd start",                                                              //start server
+      "echo Welcome Badalo - VS is at ${self.public_dns} | sudo tee /var/www/html/index.html", //copy a file
+    ]
+  }
 }
--- 05-ec2-instances/outputs.tf
+++ 05-ec2-instances/outputs.tf
@@ -2,3 +2,8 @@
 output "aws_security_group_http_server_details" {
   value = aws_security_group.http_server_sg
 }
+
+output "aws_instance" {
+  value = aws_instance.http_server
+}
+
