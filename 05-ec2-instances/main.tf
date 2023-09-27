a579f3e03fab6b8aac7e9b2fc29c04cab62daac0
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-27 20:13:38 +0100

    04 Backup & create EC2 configuration

---
+++ 05-ec2-instances/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+  # VERSION IS NOT NEEDED HERE
+  #Put access to the provider via environment variable instead of hardcoded -> https://registry.terraform.io/providers/hashicorp/aws/latest/docs 
+}
+
+//http Server -> 80 TCP, 22 TCP, CIDR ["0.0.0.0/0"]
+
+//Security Group
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = "vpc-0cc097fd81330d115"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+
+}
+
+resource "aws_instance" "http_server" {
+  ami                        = "ami-0b25f6ba2f4419235"
+  key_name                   = "default-ec2"
+  instance_type              = "t2.micro"
+  vpc_vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id                  = "subnet-0ab30df0d626c6175"
+}
---
+++ 05-ec2-instances/outputs.tf
@@ -0,0 +1,4 @@
+//Output variables
+output "aws_security_group_http_server_details" {
+  value = aws_security_group.http_server_sg
+}
