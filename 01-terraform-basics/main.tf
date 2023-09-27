cb29cd94bb1fe8f6c1e27af13502e2675376ec13
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-27 18:27:52 +0100

    Refactoring files to execute

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -17,6 +17,7 @@ provider "aws" {
 
 #Plan -> Apply
 
+##Resources
 ##Create bucket/Storage
 resource "aws_s3_bucket" "my_s3_bucket" {
     bucket = "my-s3-bucket-badalo-123"
@@ -32,5 +33,4 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
 resource "aws_iam_user" "my_iam_user" {
     name = "my_iam_user_abc_updated2"
 }
-
 
--- 01-terraform-basics/outputs.tf
+++ 01-terraform-basics/outputs.tf
@@ -1,3 +1,4 @@
+//Output variables
 output "my_s3_bucket_versioning" {
     value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
 }
