9dbd8c08c78b498483a6ef1b95b1a2d5661a1d80
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-28 00:22:34 +0100

    Creating Security Group and Classic Load Balancer

--- 06-ec2-with-elb/main.tf
+++ 06-ec2-with-elb/main.tf
@@ -27,7 +27,6 @@ resource "aws_default_vpc" "default" {
 
 resource "aws_security_group" "http_server_sg" {
   name = "http_server_sg"
-  //vpc_id = "vpc-0cc097fd81330d115"
   vpc_id = aws_default_vpc.default.id
   ingress {
     from_port   = 80
@@ -55,6 +54,41 @@ resource "aws_security_group" "http_server_sg" {
   }
 
 }
+
+resource "aws_security_group" "elb_sg" {
+  name = "elb_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_elb" "elb" {
+  name = "elb"
+  subnets = data.aws_subnets.default_subnets.ids
+  security_groups = [aws_security_group.elb_sg.id]
+  instances = values(aws_instance.http_servers).*.id
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  }
 
 resource "aws_instance" "http_servers" {
   ami                    = data.aws_ami.aws_linux_2023_latest.id
