75a168c742c06629262872209f06b82adc0ef219
LuisBadal0 <luismbbadalo@outlook.com>
2023-09-28 01:03:14 +0100

    Create multiple environments with workspaces

--- 07-backend-state/users/main.tf
+++ 07-backend-state/users/main.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    bucket = "dev-applications-backend-state-badalo"
+    #key = "07-backend-state-users-dev"
+    key = "07-backend-state/users/backend-states"
+    region = "eu-west-2"
+    dynamodb_table = "dev_application_locks"
+    encrypt = true
+    
+  }
 
 }
 
@@ -19,6 +28,6 @@ provider "aws" {
 
 
 resource "aws_iam_user" "my_iam_user" {
-  name = "my_iam_user_abc"
+  name = "${terraform.workspace}_my_iam_user_abc"
 }
 
